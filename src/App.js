68f1ffc0e1acd826db20c71837153b7f6f5ccbb6
PixelCook <zacharyigould@gmail.com>
2021-01-20 12:34:40 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Main from "../src/pages/mainpage"
 
 function App() {
+  require('dotenv').config()
+  console.log()
   return (
     <div>
       <Main/>
---
+++ src/components/location.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Background from "../backgrounddesigns/3.svg"
+import Map from "../components/googlemap"
+
+export default function location(){
+    <div
+    className="background"
+    style={{
+      backgroundImage: `url(${Background})`,
+      backgroundRepeat: "no-repeat",
+      height: "100vh",
+      backgroundSize: "100% 100%",
+    }}
+  >
+     <Map/>
+  </div>
+
+
+}
