374ef1d53e8fb50f0ea4991e0a32063397b7d1e8
PixelCook <zacharyigould@gmail.com>
2021-01-20 12:56:43 +0200

    update

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -1,19 +1,19 @@
-import React from "react"
-import Background from "../backgrounddesigns/3.svg"
-import Map from "../components/googlemap"
+import React from "react";
+import Background from "../backgrounddesigns/3.svg";
+import Map from "../components/googlemap";
 
-export default function location(){
+export default function location() {
+  return (
     <div
-    className="background"
-    style={{
-      backgroundImage: `url(${Background})`,
-      backgroundRepeat: "no-repeat",
-      height: "100vh",
-      backgroundSize: "100% 100%",
-    }}
-  >
-     <Map/>
-  </div>
-
-
+      className="background"
+      style={{
+        backgroundImage: `url(${Background})`,
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        backgroundSize: "100% 100%",
+      }}
+    >
+      <Map />
+    </div>
+  );
 }
