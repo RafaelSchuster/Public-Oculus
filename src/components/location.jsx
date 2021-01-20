50d788e4bf25def2c9fd8f6cac383386fb694a31
PixelCook <zacharyigould@gmail.com>
2021-01-21 01:03:48 +0200

    formatting and maps

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Background from "../backgrounddesigns/3.svg";
-import Map from "../components/googlemap";
+import "../css/map.css";
 
 export default function location() {
   return (
@@ -13,8 +13,14 @@ export default function location() {
         backgroundSize: "100% 100%",
       }}
     >
-      <Map />
+      <h1 className="title">Here are some providers in the area</h1>
+
+      <div
+        className="map"
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d54094.63262875113!2d34.76284698941475!3d32.07156013207808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1soptometrists!5e0!3m2!1sen!2sil!4v1611173531866!5m2!1sen!2sil width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"/>`,
+        }}
+      />
     </div>
   );
 }
-
